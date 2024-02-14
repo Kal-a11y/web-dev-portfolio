b702359d268f883eefb3b9b5f7c07eb98078403c
Kaila <kailaamia@gmail.com>
2024-02-14 15:32:33 -0600

    Added nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Nav from './components/Nav'
 // import './App.css'
 
 function App() {
 
   return (
-      <h1>Hello world</h1>
+      <Nav />
   
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Kaila Anderson</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">About me</Nav.Link>
+            <Nav.Link href="/porfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/contact">Contact me</Nav.Link>
+            <Nav.Link href="/resume">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
