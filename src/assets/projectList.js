0fc5c087091965a0819fe66ab6797f00254e9049
Kaila <kailaamia@gmail.com>
2024-02-17 11:26:00 -0600

    Added project data

---
+++ src/assets/projectList.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        "title":"Beat Box Office",
+        "deployLink": "https://kal-a11y.github.io/beat-box-office/",
+        "repoLink":"https://github.com/Kal-a11y/beat-box-office",
+        "image": "https://raw.githubusercontent.com/Kal-a11y/professional-portfolio/main/assets/images/work-beat-box-office.PNG"
+
+    },
+    {
+        "title":"Note Taker",
+        "deployLink": "https://obscure-garden-95810-97ef6913385c.herokuapp.com/",
+        "repoLink":"https://github.com/Kal-a11y/note-taker",
+        "image": "https://github.com/Kal-a11y/note-taker/raw/main/public/assets/images/example-img-1.PNG"
+
+    },
+    {
+        "title":"Horiseon Refactoring",
+        "deployLink": "https://kal-a11y.github.io/horiseon-implement-accessibility/",
+        "repoLink":"https://github.com/Kal-a11y/horiseon-implement-accessibility",
+        "image": "https://github.com/Kal-a11y/professional-portfolio/blob/main/assets/images/work-challenge-1-project.png?raw=true"
+
+    },
+    {
+        "title":"Prework Study Guide",
+        "deployLink": "https://kal-a11y.github.io/prework-study-guide/",
+        "repoLink":"https://github.com/Kal-a11y/prework-study-guide",
+        "image": "https://github.com/Kal-a11y/professional-portfolio/blob/main/assets/images/work-prework-project.png?raw=true"
+
+    },
+    {
+        "title":"Career Pal Job Search",
+        "deployLink": "https://career-pal-project-2-5fb6730b0a09.herokuapp.com/",
+        "repoLink":"https://github.com/Siennaxstarr/career-pal",
+        "image": "https://github.com/Kal-a11y/professional-portfolio/blob/main/assets/images/work-career-pal.PNG?raw=true"
+
+    },
+    {
+        "title":"Weather Dashboard",
+        "deployLink": "https://kal-a11y.github.io/weather-dashboard/",
+        "repoLink":"https://github.com/Kal-a11y/weather-dashboard",
+        "image": "https://github.com/Kal-a11y/weather-dashboard/raw/main/Assets/images/example-img-1.PNG"
+
+    },
+]
