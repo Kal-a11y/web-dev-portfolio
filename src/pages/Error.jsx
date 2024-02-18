b5be957b9ed3b36c273f64533e58d4dbe5041725
Kaila <kailaamia@gmail.com>
2024-02-18 16:13:16 -0600

    Added basic error elements

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,9 @@
+import { Link } from 'react-router-dom';
 export default function Error() {
     return(
         <div>
+            <h1>404 Sorry page not found.</h1>
+            <Link to='/'>Click here to return to home page.</Link>
             
         </div>
     )
