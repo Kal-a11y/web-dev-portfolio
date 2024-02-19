a894e19d61bfc3156111ae103f1489717d56eaa9
Kaila <kailaamia@gmail.com>
2024-02-19 14:49:37 -0600

    Removed server

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000,
-    open: true,
-  }
 })
